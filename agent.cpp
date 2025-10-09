3e92d7d395213f75444bf9891be610f1c0639d05
Federico Greco <fedegreco07@gmail.com>
2025-10-09 00:44:17 +0000

    HANDLE FREAKING TURNOOOOO (se mueve al norte :) )

--- agent.cpp
+++ agent.cpp
@@ -4,21 +4,42 @@
 
 using namespace std;
 
-int main() {
+int main(int argc, char* argv[]) {
+    string host = "127.0.0.1";
+    int port = 8080;
+    string agent_id = "backup_agent_id";
+
+    if (argc > 1) {
+        host = argv[1];
+    }
+    if (argc > 2) {
+        port = stoi(argv[2]);
+    }
+    if (argc > 3) {
+        agent_id = argv[3];
+    }
+
     // GLHF ;) (no está fun profe)
 
     net::TcpConnection connection;
-    string host = "127.0.0.1";
-    connection.connect(host, 8080);
-    string message = "{"
+    connection.connect(host, port);
+    string register_agent_message = "{"
         "\"id\":\"1\","
         "\"type\":\"register_agent\","
-        "\"agent_id\":\"blue_agent_001\""
+        "\"agent_id\":" + agent_id +
         "}";
-    connection.sendMessage(message);
-    string response = connection.receiveMessage();
-    string actual_response = rpc::escapeJson(response);
-    cout << actual_response << endl;
+    connection.sendMessage(register_agent_message);
+    while (true) {
+        string response = connection.receiveMessage();
+        string id = rpc::extractStringValue(response, "id");
+        if (rpc::extractStringValue(response, "type") == "play_turn") {
+            string turn_message = "{"
+                "\"id\":" + id + ","
+                "\"action\":\"move north\""
+                "}";
+            connection.sendMessage(turn_message);
+        }
+    }
 
 
     return 0;
--- mock_server.cpp
+++ mock_server.cpp
@@ -6,15 +6,27 @@ using namespace std;
 
 int main() {
 
-    net::TcpServer server;
-    server.start();
-    unique_ptr<net::TcpConnection> connection = server.acceptConnection();
-    string message = connection->receiveMessage();
     string void_response = "{"
         "\"id\":\"1\","
         "\"status\":\"ok\""
         "}";
+
+    net::TcpServer server;
+    server.start();
+    unique_ptr<net::TcpConnection> connection = server.acceptConnection();
+    string message = connection->receiveMessage();
+    string agent_id = rpc::extractStringValue(message, "agent_id");
     connection->sendMessage(void_response);
+
+    string turn_message = "{"
+        "\"id\":\"1\","
+        "\"type\":\"play_turn\","
+        "\"agent_id\":" + agent_id +
+        "}";
+
+    connection->sendMessage(turn_message);
+    message = connection->receiveMessage();
+    string move = rpc::extractStringValue(message, "action");
 
     return 0;
 }
